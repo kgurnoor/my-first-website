62370616e32e56a6071f42aba9a14298ade1cf8d
kgurnoor <gurnoorkaur0349@gmail.com>
2025-04-10 11:26:12 +0530

    Cleared the home page to accomodate blogs in the blog section

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Page() {
       <p className="mb-4">
         {`I am an aspiring Software Developing Engineer. This is my website that I created to post weekly blogs and projects. This is the exhibition of my learnings so far and to inspire other women coders to excell in the field of computer science. This website is helping me to keep track of my progress too`}
       </p>
-      <div className="my-8">
+      {/* <div className="my-8">
         <BlogPosts />
-      </div>
+      </div> */}
     </section>
   )
 }
